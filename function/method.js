84f974b34bfda6568c72d27510b17f6ada8e45a9
yazshrilld <yazshrilld@gmail.com>
2023-08-21 08:02:26 +0100

    using the filter, find, findIndex on solutions

--- function/method.js
+++ function/method.js
@@ -310,4 +310,28 @@ let YazCorpSearch = "Done"
 
 const YAZ = `otp/send/otpUrl=https://www.providusbank.com?musa.yazid49@gmail.com`
 const YAZI = `otp/send/musa.yazid49@gmail.com?otpUrl=https://www.providusbank.com`
-console.log("Check: ", YAZ === YAZI)
+// console.log("Check: ", YAZ === YAZI)
+const array1 = [5, 123, 8, 130, 44];
+
+const isLargeNumber = (element) => element > 13;
+const value = array1.findIndex(isLargeNumber)
+
+
+// console.log("Value: ", array1[value]);
+
+const array2 = [5, 12, 8, 130, 44];
+
+const found = array2.some((element) => element > 10);
+
+// console.log(found);
+// Expected output: true
+
+//Notes On Finding Values Inside An Array->
+// The find() method of Array instances returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+
+// If you need the index of the found element in the array, use findIndex().
+// If you need to find the index of a value, use indexOf(). (It's similar to findIndex(), but checks each element for equality with the value instead of using a testing function.)
+// If you need to find if a value exists in an array, use includes(). Again, it checks each element for equality with the value instead of using a testing function.
+// If you need to find if any element satisfies the provided testing function, use some().
+
+console.log("result: ", 0 > 0);
